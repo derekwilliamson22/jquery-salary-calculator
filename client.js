cd630f2486e9d4e7683d5a741e0dbad89953113d
Derek Williamson <derekwilliamson22@gmail.com>
2020-08-29 00:35:31 -0500

    adding table

---
+++ client.js
@@ -0,0 +1,40 @@
+$(document).ready(onReady);
+console.log('locked in');
+
+
+function onReady(){
+
+
+
+
+
+
+
+
+} // end onReady
+
+/*function pageLoad(){
+console.log('hello pageLoad');
+
+let inputInfo = [ 
+     { id: 'firstName', placeholder: `FirstName` },
+     { id: 'lastName', placeholder: `LastName` },
+     { id: 'idNumber', placeholder: `IDNumber` },
+     { id: 'jobTitle', placeholder: `JobTitle` },
+     { id: 'annualSalary', placeholder: `AnnualSalary` }
+];
+
+console.log(inputInfo);
+
+for (let i = 0; i < inputInfo.length; i++) {
+    console.log(`${inputInfo[i].placeholder}`);
+    
+   $(`.addEmployee`).append( `
+    <input id=${inputInfo[i].id} type="text" placeholder=${inputInfo[i].placeholder}>
+    ` );
+}
+
+
+
+
+} // end pageLoad */
